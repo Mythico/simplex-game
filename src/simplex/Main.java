b1ed00bdada6413224cfdb3297fe1d584b9f5985
samuelmannehed@gmail.com <samuelmannehed@gmail.com@8e56639f-f80c-9917-6b4d-28a840210550>
2012-09-04 12:30:18 +0000

    Added Slick2d and lwjgl library.

--- src/simplex/Main.java
+++ src/simplex/Main.java
@@ -1,5 +1,46 @@
 package simplex;
 
-public class Main {
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
+
+public class Main extends BasicGame{
+	
+ 
+    public Main()
+    {
+        super("Slick2DPath2Glory - SimpleGame");
+    }
+ 
+    @Override
+    public void init(GameContainer gc) 
+			throws SlickException {
+ 
+    }
+ 
+    @Override
+    public void update(GameContainer gc, int delta) 
+			throws SlickException     
+    {
+ 
+    }
+ 
+    public void render(GameContainer gc, Graphics g) 
+			throws SlickException 
+    {
+ 
+    }
+ 
+    public static void main(String[] args) 
+			throws SlickException
+    {
+         AppGameContainer app = 
+			new AppGameContainer(new Main());
+ 
+         app.setDisplayMode(800, 600, false);
+         app.start();
+    }
 }
