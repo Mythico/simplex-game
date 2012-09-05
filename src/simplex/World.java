3ba8442cc326588ec89816c8db063f77a9f65dfc
martensson.emil@gmail.com <martensson.emil@gmail.com@8e56639f-f80c-9917-6b4d-28a840210550>
2012-09-05 15:19:27 +0000

    Added a World class.

---
+++ src/simplex/World.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplex;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+
+/**
+ *
+ * @author Emil
+ */
+public class World {
+
+    void update(GameContainer gc, int delta) throws SlickException{
+        
+    }
+    
+}
