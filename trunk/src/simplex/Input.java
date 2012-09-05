f699d392e552d74384a69fb337014725a6481d29
martensson.emil@gmail.com <martensson.emil@gmail.com@8e56639f-f80c-9917-6b4d-28a840210550>
2012-09-05 15:19:46 +0000

    Added a few classes.

---
+++ trunk/src/simplex/Input.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplex;
+
+/**
+ *
+ * @author Emil
+ */
+public class Input {
+
+    
+    
+    public Input(World world) {
+        
+    }
+    
+}
---
+++ trunk/src/simplex/entity/Consumer.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplex.entity;
+
+/**
+ *
+ * @author Emil
+ */
+public class Consumer {
+    
+}
