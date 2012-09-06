04572afa94a0a8a114a7d97c4c6af5963f00b59d
martensson.emil@gmail.com <martensson.emil@gmail.com@8e56639f-f80c-9917-6b4d-28a840210550>
2012-09-06 08:04:43 +0000

    Added Connection.java Changed Node so it can use Connection.java Changed GameState with some basic "game" drawings.

--- trunk/src/simplex/GameState.java
+++ trunk/src/simplex/GameState.java
@@ -4,11 +4,17 @@
  */
 package simplex;
 
+import java.util.LinkedList;
+import java.util.List;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import simplex.entity.Connection;
+import simplex.entity.Consumer;
+import simplex.entity.Factory;
+import simplex.entity.Node;
 
 /**
  *
@@ -17,6 +23,8 @@ import org.newdawn.slick.state.StateBasedGame;
 public class GameState extends BasicGameState{
     
     private final int stateId;
+    
+    private List<Node> nodes = new LinkedList<>();
 
     public GameState(int stateId) {
         this.stateId = stateId;
@@ -30,17 +38,39 @@ public class GameState extends BasicGameState{
 
     @Override
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+    
+        Node n1 = new Node(gc);
+        Node n2 = new Node(gc);
+        Node n3 = new Node(gc);
+        Node n4 = new Node(gc);
         
+        new Connection(gc, n1, n2);
+        new Connection(gc, n2, n3);
+        new Connection(gc, n2, n4);
+        new Connection(gc, n3, n4);
+        
+        n1.setLocation(100, 100);
+        n2.setLocation(200, 100);
+        n3.setLocation(100, 200);
+        n4.setLocation(200, 200);
+        
+        nodes.add(n1);
+        nodes.add(n2);
+        nodes.add(n3);
+        nodes.add(n4);
     }
 
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) 
             throws SlickException {
-        g.drawString("Game", 10, 100);
+        for(Node node : nodes){
+            node.render(gc, g);
+        }
+        
     }
 
     @Override
-    public void update(GameContainer gc, StateBasedGame sbg, int i) 
+    public void update(GameContainer gc, StateBasedGame sbg, int delta) 
             throws SlickException {
         
     }
--- trunk/src/simplex/entity/Node.java
+++ trunk/src/simplex/entity/Node.java
@@ -4,10 +4,68 @@
  */
 package simplex.entity;
 
+import java.util.LinkedList;
+import java.util.List;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.gui.AbstractComponent;
+import org.newdawn.slick.gui.GUIContext;
+
 /**
  *
  * @author Emil
  */
-public class Node {
-    
+public class Node extends AbstractComponent {
+
+    private int x, y, width, height;
+    private List<Connection> connections = new LinkedList<>();
+
+    public Node(GUIContext container) {
+        super(container);
+    }
+
+    @Override
+    public void render(GUIContext guic, Graphics g) throws SlickException {
+        //Render the node;
+        
+        
+        
+        for (Connection conn : connections) {
+            conn.render(guic, g);
+        }
+    }
+
+    @Override
+    public void setLocation(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    public void removeConnection(Connection conn) {
+        connections.remove(conn);
+    }
+
+    public void addConnection(Connection conn) {
+        connections.add(conn);
+    }
 }
