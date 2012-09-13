91f898d8659d048f0ca95980e255f38d3b4e0a35
martensson.emil@gmail.com <martensson.emil@gmail.com@8e56639f-f80c-9917-6b4d-28a840210550>
2012-09-13 12:22:54 +0000

    Removed template comments.

--- src/simplex/entity/Entity.java
+++ src/simplex/entity/Entity.java
@@ -1,13 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package simplex.entity;
 
 import org.newdawn.slick.Graphics;
 
 /**
- *
+ * 
  * @author Emil
  * @author Samuel
  */
