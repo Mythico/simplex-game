60dd58236c6d995ef9f985522ee843b22a29f04f
martensson.emil@gmail.com <martensson.emil@gmail.com@8e56639f-f80c-9917-6b4d-28a840210550>
2012-09-16 09:17:51 +0000

    Reworked how the NodeFactory works. It now is a singleton that also stores all the created nodes. Also added GridCoord.java for encapsulation grid coordinates.
    Note: Connection doesn't lie inside of nodefactory and therefor must be added to its own list.
    I leave this for a future change since connection should also be reworked.

---
+++ trunk/src/simplex/util/GridCoord.java
@@ -0,0 +1,42 @@
+package simplex.util;
+
+/**
+ * A simple encapsulation of two integers that will represent grid coordinates.
+ * @author Emil
+ * @author Samuel
+ */
+public class GridCoord {
+    public int x;
+    public int y;
+
+    public GridCoord(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + this.x;
+        hash = 83 * hash + this.y;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final GridCoord other = (GridCoord) obj;
+        if (this.x != other.x) {
+            return false;
+        }
+        if (this.y != other.y) {
+            return false;
+        }
+        return true;
+    }    
+}
