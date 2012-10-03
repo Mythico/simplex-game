502bbf8c3a9df835c7afd512143411b5171d5179
martensson.emil@gmail.com <martensson.emil@gmail.com@8e56639f-f80c-9917-6b4d-28a840210550>
2012-10-03 08:10:03 +0000

    Moved all SavedEntities to their own subfolder.

---
+++ src/simplex/level/saved/SavedConnection.java
@@ -0,0 +1,50 @@
+package simplex.level.saved;
+
+import simplex.entity.Connection;
+import simplex.entity.Node;
+import simplex.entity.NodeFactory;
+
+/**
+ *
+ * @author Emil
+ * @author Samuel
+ */
+public class SavedConnection extends SavedEntity {
+    private int startNodeId;
+    private int endNodeId;
+
+    public SavedConnection() {
+    }
+
+    SavedConnection(int startNodeId, int endNodeId, int id) {
+        super(id);
+        this.startNodeId = startNodeId;
+        this.endNodeId = endNodeId;
+    }
+
+    public int getStartNodeId() {
+        return startNodeId;
+    }
+
+    public void setStartNodeId(int startNodeId) {
+        this.startNodeId = startNodeId;
+    }
+
+    public int getEndNodeId() {
+        return endNodeId;
+    }
+
+    public void setEndNodeId(int endNodeId) {
+        this.endNodeId = endNodeId;
+    }
+    
+    @Override
+    public Connection create(){
+        Connection conn = new Connection();
+        Node n1 = SavedNodeFactory.instance().getNode(startNodeId);
+        Node n2 = SavedNodeFactory.instance().getNode(endNodeId);
+        NodeFactory.instance().bind(n1, n2, conn);
+        return conn;
+    }
+    
+}
---
+++ src/simplex/level/saved/SavedNode.java
@@ -0,0 +1,45 @@
+package simplex.level.saved;
+
+import simplex.entity.Node;
+import simplex.util.GridCoord;
+
+/**
+ *
+ * @author Emil
+ * @author Samuel
+ */
+public abstract class SavedNode extends SavedEntity  {
+
+    private int id;
+    private GridCoord coord;
+
+    public SavedNode() {
+    }
+
+    SavedNode(GridCoord coord, int id) {
+        super(id);
+        this.coord = coord;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public GridCoord getCoord() {
+        return coord;
+    }
+
+    public void setCoord(GridCoord coord) {
+        this.coord = coord;
+    }
+
+    
+
+    @Override
+    public abstract Node create();
+    
+}
