d4c3e4535d104c51c4a636e2af66f1635a95d25b
martensson.emil@gmail.com <martensson.emil@gmail.com@8e56639f-f80c-9917-6b4d-28a840210550>
2012-10-04 16:44:37 +0000

    Added a simple timer and clickcounter to the game

--- src/simplex/GameState.java
+++ src/simplex/GameState.java
@@ -3,6 +3,7 @@ package simplex;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import mdes.oxy.Label;
 import mdes.oxy.Panel;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -24,8 +25,12 @@ import simplex.util.ImageManager;
  * @author Samuel
  */
 public class GameState extends EngineState {
+
     private List<MouseOverArea> tempConnSwap = new LinkedList<>();
-    private GameContainer gc;    
+    private GameContainer gc;
+    private long startTime;
+    private int clicks;
+    private long endTime;
 
     public GameState(int stateId) {
         super(stateId);
@@ -37,13 +42,13 @@ public class GameState extends EngineState {
         super.init(gc, sbg);
         this.gc = gc;
         loadGui(gc, "GameGui.xml");
-        
+
     }
 
     @Override
     public void enter(GameContainer container, StateBasedGame game) throws SlickException {
         super.enter(container, game);
-        
+
         List<Connection> connections = level.getConnections();
         tempConnSwap.clear();
         for (Connection conn : connections) {
@@ -53,28 +58,37 @@ public class GameState extends EngineState {
         }
         Panel panel = getGuiComponent("ScorePanel");
         panel.setVisible(false);
-    }    
-    
+        startTime = System.currentTimeMillis();
+        endTime = -1;
+        clicks = 0;
+    }
+
     @Override
     protected void renderContent(GameContainer gc, StateBasedGame sbg,
             Graphics g) {
-        super.renderContent(gc, sbg, g);        
+        super.renderContent(gc, sbg, g);
         for (MouseOverArea moa : tempConnSwap) {
             moa.render(gc, g);
         }
     }
 
     @Override
-    public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
-           
+    public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {        
+        super.update(gc, sbg, delta);
+        if (delta == 0 || endTime != -1) {
+            return;
+        }
+        
         if (levelIsDone()) {
             Panel panel = getGuiComponent("ScorePanel");
+            endTime = (System.currentTimeMillis() - startTime) / 1000;
+            this.<Label>getGuiComponent("time").setText("" + endTime);
+            this.<Label>getGuiComponent("clicks").setText("" + clicks);
             panel.setVisible(true);
         }
-        super.update(gc, sbg, delta);
-        
+
         List<Connection> connections = level.getConnections();
-        
+
         //TODO: Make the switching a part of connection
         if (gc.getInput().isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
             for (int i = 0; i < tempConnSwap.size(); i++) {
@@ -84,42 +98,47 @@ public class GameState extends EngineState {
             }
         }
     }
-    
-    
 
     @Override
     public void keyReleased(int key, char c) {
         if (Input.KEY_P == key || Input.KEY_PAUSE == key) {
             gc.setPaused(!gc.isPaused());
-        } else if(Input.KEY_ESCAPE == key){
-            setNextState(Main.MAINMENUSTATE);            
+        } else if (Input.KEY_ESCAPE == key) {
+            setNextState(Main.MAINMENUSTATE);
         }
     }
+
+    @Override
+    public void mouseReleased(int button, int x, int y) {
+        clicks++;
+    }
+    
+    
 
     private boolean levelIsDone() {
         Map<GridCoord, Node> nodes = level.getNodes();
         //TODO: do a better check.
         for (Node node : nodes.values()) {
             NodeSpecification spec = node.getNodeSpecification();
-            if (spec instanceof ConsumerSpecification && 
-                    !((ConsumerSpecification) spec).isHappy()) {
+            if (spec instanceof ConsumerSpecification
+                    && !((ConsumerSpecification) spec).isHappy()) {
                 return false;
             }
         }
         return true;
     }
-    
+
     /**
      * Used by the GUI to switch to the next level.
      */
-    public void goToNext(){
+    public void goToNext() {
         setNextState(Main.NEXT_GAME);
     }
-    
+
     /**
      * Used by the GUI to switch to main menu.
      */
-    public void goToMain(){
+    public void goToMain() {
         setNextState(Main.MAINMENUSTATE);
     }
 }
