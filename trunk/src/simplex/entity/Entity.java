0c21d3706333ae65f0e5b4d5b3c4aa838a3177d2
martensson.emil@gmail.com <martensson.emil@gmail.com@8e56639f-f80c-9917-6b4d-28a840210550>
2012-10-15 14:46:34 +0000

    Added some JavaDoc

--- trunk/src/simplex/entity/Entity.java
+++ trunk/src/simplex/entity/Entity.java
@@ -8,6 +8,17 @@ import org.newdawn.slick.Graphics;
  * @author Samuel
  */
 public interface Entity {
+    
+    /**
+     * Update the level
+     * @param delta Time from last update.
+     */
     public void update(int delta);
+    
+    
+    /**
+     * Render the Entity.
+     * @param g The graphics the level will be rendered with.
+     */
     public void render(Graphics g);
 }
