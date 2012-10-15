2d40ae4f913abf6fb17e63e9679ebf3666a69367
martensson.emil@gmail.com <martensson.emil@gmail.com@8e56639f-f80c-9917-6b4d-28a840210550>
2012-10-15 15:27:08 +0000

    Added some JavaDoc.

--- trunk/src/simplex/level/saved/SavedNode.java
+++ trunk/src/simplex/level/saved/SavedNode.java
@@ -4,6 +4,7 @@ import simplex.entity.Node;
 import simplex.util.GridCoord;
 
 /**
+ * An abstract class for all the nodes that will be serialized to YAML objects.
  *
  * @author Emil
  * @author Samuel
@@ -12,22 +13,42 @@ public abstract class SavedNode extends SavedEntity  {
 
     private GridCoord coord;
 
+    /**
+     * An empty constructor for the YAML serialization.
+     */
     public SavedNode() {
     }
 
+    /**
+     * Creates a new SavedNode with coordinates and an id.
+     * @param coord The coordinates node that is being saved.
+     * @param id  The id of the node that is being saved.
+     */
     public SavedNode(GridCoord coord, int id) {
         super(id);
         this.coord = coord;
     }
 
+    /**
+     * Get the coordinates.
+     * @return The coordinates.
+     */
     public GridCoord getCoord() {
         return coord;
     }
 
+    /**
+     * Set the coordinates.
+     * @param coord The coordinates to be set.
+     */
     public void setCoord(GridCoord coord) {
         this.coord = coord;
     }
-
+    
+    /**
+     * Create a node from a SavedNode.
+     * @return A new Node.
+     */
     @Override
     public abstract Node create();
     
