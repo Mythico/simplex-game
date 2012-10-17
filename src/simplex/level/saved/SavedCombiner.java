168805e0f380d9f08bb33a90481d2ea7bf972b68
martensson.emil@gmail.com <martensson.emil@gmail.com@8e56639f-f80c-9917-6b4d-28a840210550>
2012-10-17 14:05:45 +0000

    Removed unused variables.

--- src/simplex/level/saved/SavedCombiner.java
+++ src/simplex/level/saved/SavedCombiner.java
@@ -2,8 +2,6 @@ package simplex.level.saved;
 
 import simplex.entity.Node;
 import simplex.entity.NodeFactory;
-import simplex.entity.specification.CombinerSpecification;
-import simplex.entity.specification.NodeSpecification;
 
 /**
  *
@@ -11,22 +9,18 @@ import simplex.entity.specification.NodeSpecification;
  * @author Samuel
  */
 public class SavedCombiner extends SavedNode {
-    private int fraction;
 
     private SavedCombiner() {
     }
 
     public SavedCombiner(Node node) {
         super(node);
-        NodeSpecification spec = node.getNodeSpecification();
-        CombinerSpecification combinerSpecification = (CombinerSpecification) spec;
     }
 
     @Override
     public Node create() {
         Node node = NodeFactory.instance().createCombinerNode();
-        node.setGridPosition(getCoord());
-        CombinerSpecification spec = (CombinerSpecification) node.getNodeSpecification();
+        node.setGridPosition(getCoord());        
         return node;
     }    
 }
