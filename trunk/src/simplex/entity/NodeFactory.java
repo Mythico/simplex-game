927aff50e0ab91e1e61891cb1656c3e7700aaf0d
martensson.emil@gmail.com <martensson.emil@gmail.com@8e56639f-f80c-9917-6b4d-28a840210550>
2012-10-24 16:53:16 +0000

    Added JavaDoc.

--- trunk/src/simplex/entity/NodeFactory.java
+++ trunk/src/simplex/entity/NodeFactory.java
@@ -8,8 +8,10 @@ import simplex.entity.specification.FactorySpecification;
 import simplex.entity.specification.SplitterSpecification;
 
 /**
+ * A Factory used for creating nodes.
  *
  * @author Emil
+ * @author Samuel
  */
 public class NodeFactory {
 
@@ -25,30 +27,58 @@ public class NodeFactory {
         return nodeFactory;
     }
 
+    /**
+     * Create a Factory Node.
+     * @return A Node with FactorySpecification.
+     */
     public Node createFactoryNode() {
         return new Node(new FactorySpecification());
     }
 
+    /**
+     * Create a Dummy Node.
+     * @return A Node with DummySpecification.
+     */
     public Node createDummyNode() {
         return new Node(new DummySpecification());
     }
 
+    /**
+     * Create a Consumer Node.
+     * @return A Node with ConsumerSpecification.
+     */
     public Node createConsumerNode() {
         return new Node(new ConsumerSpecification());
     }
 
+    /**
+     * Create a Eater Node.
+     * @return A Node with EaterSpecification.
+     */
     public Node createEaterNode() {
         return new Node(new EaterSpecification());
     }
 
+    /**
+     * Create a Splitter Node.
+     * @return A Node with SplitterSpecification.
+     */
     public Node createSplitterNode() {
         return new Node(new SplitterSpecification());
     }
     
+    /**
+     * Create a Combiner Node.
+     * @return A Node with CombinerSpecification.
+     */
     public Node createCombinerNode() {
         return new Node(new CombinerSpecification());
     }
 
+    /**
+     * Create a new id.
+     * @return A new unique Id.
+     */
     public int getNewId() {
         return id++;
     }
