3d9068c361ff297676dd8f54c60d6a101ad99d9c
martensson.emil@gmail.com <martensson.emil@gmail.com@8e56639f-f80c-9917-6b4d-28a840210550>
2012-10-24 16:33:09 +0000

    Added JavaDoc.

--- src/simplex/util/GridConversions.java
+++ src/simplex/util/GridConversions.java
@@ -3,6 +3,7 @@ package simplex.util;
 import org.newdawn.slick.geom.Vector2f;
 
 /**
+ * A utility class for converting between different coordinate system.
  *
  * @author Emil
  * @author Samuel
@@ -16,16 +17,33 @@ public class GridConversions {
     private static float gridWidth = 0;
     private static float gridHeight = 0;
 
+    /**
+     * Set the size of the screen. This size is used for calculating the
+     * different coordinates.
+     * @param width With of the screen.
+     * @param height Height of the screen.
+     */
     public static void setScreenSize(int width, int height) {
         screenWidth = width;
         screenHeight = height;
     }
 
+    /**
+     * Set the game size. The game size defines how many nodes in a row
+     * there can be on the screen.
+     * @param width Number of nodes horizontal.
+     * @param height  Number of nodes vertical.
+     */
     public static void setGameSize(int width, int height) {
         gameWidth = width;
         gameHeight = height;
     }
 
+    /**
+     * Convert from logical GridCoord to actual screen coord.
+     * @param coord The coordinate to be converted from.
+     * @return The actual position in the window.
+     */
     public static Vector2f gridToScreenCoord(GridCoord coord) {
         float fx = coord.x * (screenWidth / (float) gameWidth);
         float fy = coord.y * (screenHeight / (float) gameHeight);
@@ -45,6 +63,10 @@ public class GridConversions {
         return new GridCoord(x, y);
     }
 
+    /**
+     * Get the grid width.
+     * @return The width of the grid.
+     */
     public static float getGridWidth() {
         if (gridWidth == 0) {
             gridWidth = screenWidth / gameWidth;
@@ -52,21 +74,38 @@ public class GridConversions {
         return gridWidth;
     }
 
+    /**
+     * Get the grid height.
+     * @return The height of the grid.
+     */
     public static float getGridHeight() {
         if (gridHeight == 0) {
             gridHeight = screenHeight / gameHeight;
         }
         return gridHeight;
     }
-
+    
+    /**
+     * Get the game width.
+     * @return The width of the game.
+     */
     public static int getGameWidth() {
         return gameWidth;
     }
 
+    /**
+     * Get the game height.
+     * @return The height of the game.
+     */
     public static int getGameHeight() {
         return gameHeight;
     }
 
+    /**
+     * Convert from actual screen coordinates to a grid coord.
+     * @param position The position on the screen.
+     * @return The GridCoord that represents that area of the screen.
+     */
     public static GridCoord screenToGrid(Vector2f position) {
         int x = (int) (position.x / (screenWidth / (float) gameWidth));
         int y = (int) (position.y / (screenHeight / (float) gameHeight));
